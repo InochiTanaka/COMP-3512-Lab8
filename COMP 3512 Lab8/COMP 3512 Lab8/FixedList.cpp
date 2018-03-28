945f60d4a8d5725b265d78557542e10988a118db
Inochi Tanaka <yunominosencha@gmail.com>
2018-03-28 12:02:04 -0700

    Create and Initial serring on FixedList.cpp

---
+++ COMP 3512 Lab8/COMP 3512 Lab8/FixedList.cpp
@@ -0,0 +1,68 @@
+﻿template<class T, size_t N>
+class FixedList
+{
+public:
+	FixedList();
+	~FixedList();
+	const T& get(unsigned int index) const;
+	T& operator[](unsigned int index);
+	int getFirstIndex(const T& t) const;
+	size_t size() const;
+	size_t capacity() const;
+	bool add(const T& t);
+	T remove(const T& t);
+private:
+	// private variables here
+};
+
+template<class T, size_t N>
+FixedList<T, N>::FixedList()
+{
+}
+
+template<class T, size_t N>
+FixedList<T, N>::~FixedList()
+{
+}
+
+template<class T, size_t N>
+const T & FixedList<T, N>::get(unsigned int index) const
+{
+	// TODO: insert return statement here
+}
+
+template<class T, size_t N>
+T & FixedList<T, N>::operator[](unsigned int index)
+{
+	return T();
+}
+
+template<class T, size_t N>
+int FixedList<T, N>::getFirstIndex(const T & t) const
+{
+	return 0;
+}
+
+template<class T, size_t N>
+size_t FixedList<T, N>::size() const
+{
+	return size_t();
+}
+
+template<class T, size_t N>
+size_t FixedList<T, N>::capacity() const
+{
+	return size_t();
+}
+
+template<class T, size_t N>
+bool FixedList<T, N>::add(const T & t)
+{
+	return false;
+}
+
+template<class T, size_t N>
+T FixedList<T, N>::remove(const T & t)
+{
+	return T();
+}
